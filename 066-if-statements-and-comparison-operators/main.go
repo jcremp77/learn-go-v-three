f15c74ed69e7cea6618c77d9d5543c3fe1dfdeb6
jcremp77 <jcremp77gmail.com>
2024-07-16 00:33:51 -0600

    066 updated

--- 066-if-statements-and-comparison-operators/main.go
+++ 066-if-statements-and-comparison-operators/main.go
@@ -7,7 +7,38 @@ import (
 func main() {
 	x := 10
 
+	// If statements
 	if x > 9 {
 		fmt.Println("10 is greater than 9")
 	}
+
+	if x != 9 {
+		fmt.Println("10 is not equal to 9")
+	}
+
+	if x == 10 {
+		fmt.Println("10 is equal to 10")
+	}
+
+	// If-Else statement
+	if x < 10 {
+		fmt.Printf("%v is less than 10\n", x)
+	} else {
+		fmt.Printf("%v is not less than 10\n", x)
+	}
+
+	// If-Else If-Else
+	if x >= 11 {
+		fmt.Printf("%v is greater than or equal to 11\n", x)
+	} else if x <= 9 {
+		fmt.Printf("%v is less than or equal to 9\n", x)
+	} else {
+		fmt.Printf("%v is greater than 9 but less than 11\n", x)
+	}
+
+	// If statement initialization
+	if z := 99; z >= 95 {
+		fmt.Printf("%v is greater or equal to 95", z)
+	}
+
 }
