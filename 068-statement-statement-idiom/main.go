859991f7fab3bd2027f67ed6e1cf6ee4dd620995
jcremp77 <jcremp77gmail.com>
2024-07-16 01:07:01 -0600

    068 updated

--- 068-statement-statement-idiom/main.go
+++ 068-statement-statement-idiom/main.go
@@ -8,7 +8,13 @@ import (
 
 func main() {
 	x := 20
-	//
+
+	/*
+		func Intn(n int) int
+		will choose a number between 0 - 19
+		as x above has been assigned the
+		value of 20
+	*/
 	if z := 2 * rand.Intn(x); z >= x {
 		fmt.Printf("z is %v and that is GREATER THAN OR EQUAL to x which is %v\n", z, x)
 	} else {
